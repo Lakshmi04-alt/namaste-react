d1d3362ffe2d9f51e92988256a576f25148e0032
Lakshmi Chenna <lakshmi.chennalakshmi04@gmail.com>
2025-05-10 17:05:40 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,32 @@
+   /* const heading=React.createElement
+    ("h1",
+     {id:"heading",xyz:"abc"},
+     "Hello World from React");
+    const root=ReactDOM.createRoot(document.getElementById("root"));
+    root.render(heading);
+    console.log(heading); */
+
+
+
+    /* <div id="parent">
+         <div id="child">
+           <h1>..</h1>
+           <h2>..</h2>
+           </div>
+          <div id="child2">
+           <h1>..</h1>
+           <h2>..</h2>
+           </div>
+        </div>*/
+const parent=React.createElement("div",{id:"parent"},[
+            React.createElement("div",{id:"child"},[
+                React.createElement("h1",{},"I'm an h1 tag"),
+                React.createElement("h2",{},"i'm an h2 tag")
+            ]),
+            React.createElement("div",{id:"child2"},[
+                React.createElement("h1",{},"I'm an h1 tag"),
+                React.createElement("h2",{},"i'm an h2 tag")
+            ]), 
+]);        
+ const root=ReactDOM.createRoot(document.getElementById("root"));
+ root.render(parent);
